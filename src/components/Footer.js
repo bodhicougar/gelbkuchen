b4dada4431e2b3a4023a9f67a366319a61c61e7c
bodhicougar <info@blackboxmd.fyi>
2019-11-21 20:24:15 +0100

    Adaptations.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,15 @@ import './Footer.css'
 export default () => (
   <div>
     <h2 className="taCenter">
-      Follow us{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      Follow us <a href="https://www.instagram.com/blackboxmd/">@blackboxmd</a>
     </h2>
     <br />
     <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()}. All rights reserved. Recrafted
+          by <a href="https://www.blackboxmd.fyi/">BlackBoxMediaDesign</a>.
         </span>
       </div>
     </footer>
